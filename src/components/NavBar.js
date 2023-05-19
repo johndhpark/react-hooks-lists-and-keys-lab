4974c60160e93ccc1f9b40d2a92812865003e687
John DH. Park <john.dh.park@icloud.com>
2023-05-19 13:12:53 -0700

    passed all tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navItems = links.map((link, idx) => (
+    <a key={idx} href={`#${link}`}>
+      {link}
+    </a>
+  ));
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{navItems}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techTags = technologies.map((tech, idx) => (
+    <span key={idx}>{tech}</span>
+  ));
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techTags}</div>
     </div>
   );
 }
